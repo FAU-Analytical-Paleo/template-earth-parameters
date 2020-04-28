267b4753c26b96f0edfbf616c4aa64db67a74dbb
Nussaibah Raja <nussaibah.raja.schoob@fau.de>
2020-04-28 09:47:49 +0200

    starter code added

---
+++ script.R
@@ -0,0 +1,14 @@
+# Load libraries ----------------------------------------------------------
+#load libraries you'll use here, e.g. library(forecast)
+
+
+# Load data ---------------------------------------------------------------
+dat <- read.csv("data/div_earthsystem.csv", sep=";")
+head(dat)
+
+
+# Add your analyses below -------------------------------------------------
+
+
+
+
